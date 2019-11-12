4dcc1bff9b1460d44835bf538cb7dfccfbc25fde
viliusvv <vilius.valinskis@ktu.edu>
2019-11-12 14:48:08 +0200

    Fix docker install script

--- installDocker.sh
+++ installDocker.sh
@@ -1,9 +1,10 @@
+#!/bin/sh
 # Remove docker if installed
 sudo apt-get remove docker docker-engine docker.io containerd runc
 # Update
 sudo apt-get update
 # Install required packages
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 # Add docker PGP key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # Add latest docker repository
@@ -11,9 +12,9 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 # Update again
 sudo apt-get update
 # Install docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 # Install docker compose
-sudo apt-get install docker-compose
+sudo apt-get -y install docker-compose
 
 # Test out docker
 sudo docker run hello-world
